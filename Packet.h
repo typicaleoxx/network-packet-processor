3e291612c819e4c236410072f029a4253a574deb
Joshua <js8111@gmail.com>
2026-03-24 19:39:52 -0400

    Finish Packet.h

---
+++ Packet.h
@@ -0,0 +1,33 @@
+#ifndef PACKET_H
+#define PACKET_H
+
+/*Packet.h (base class)
+data members:
+int id
+int size
+int arrivalTime
+
+functions:
+-constructor
+-getters
+-virtual process()
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Packet {
+protected:
+    int id;
+    int size;
+    int arrivalTime;
+public:
+    Packet(int id, int size, int arrivalTime) : id(id), size(size), arrivalTime(arrivalTime) {}
+    int getId() const { return id; }
+    int getSize() const { return size; }
+    int getArrivalTime() const { return arrivalTime; }
+    virtual void process() = 0;
+    virtual ~Packet() = default;
+};
+
+#endif
